fff6c654861aae960ab5f5ebc7384e649b150a39
Andrew Nelson <ajn2004@med.cornell.edu>
2020-05-05 22:20:22 -0400

    Snapshot of Succesful Peak Detection
    Quick Snapshot of the codes library after the execution of the gpu finding local maxima  in an image

--- Hurricane/Hurricane.py
+++ Hurricane/Hurricane.py
@@ -1,31 +1,162 @@
 # -*- coding: utf-8 -*-
 """
-Hurricane Function Library
+Title: Hurricane
 
-This is a temporary script file.
+Concept: This is the localization Pipeline the analyzes tiff / fits files and 
+outputs localization data for subsequent analysis
+Created on Tue May  5 10:21:58 2020
+
+@author: Andrew Nelson
 """
-import numpy as np
-from PIL import Image
+#%% Import Libraries
+from ryan_image_io import *
+from rolling_ball_subtraction import *
 from numba import cuda
+from numba import vectorize, float64
+import numpy as np
+import matplotlib.pyplot as plt
+import os
 
-def load_image_to_array(fname):
-    """This function will take in a string as a file location and file name
-       It will return a Numpy Array of the image at this location"""
-    im = Image.open(fname) #Initial call to image datafile
-    #Get shape of image
-    m,n = np.shape(im)
-    o = im.n_frames        
-    # Allocate variable memory for image
-    tiff_array = np.zeros((m,n,o))
-    for i in range(o):
-        im.seek(i) #Seek to frame i in image stack
-        tiff_array[:,:,i]= np.array(im) #Populate data into preallocated variable
-    return tiff_array # return numpy tiff array
+#%% GPU Accelerated Code
+#%%
+"""Begin GPU Code"""
+@cuda.jit()
+def gpu_peaks(image_in, image_out, thresh, pixel_width):
+    """
+    image_in.shape = image_out.shape 
+        
+    This code will look in the neighborhood of radius pixw to determine if it is a local maxima
+    
+    To do this the code will search a neighborhood in a raster scan while keep track of the 
+    maxima value it finds above the threshold provided. Once found the ith and jth coordinate are noted
+    if they correspond to the cuda i,j then the resulting image_out[i,j,k] = 1 and all rest = 0
+    
+    This is being followed from https://www.vincent-lunot.com/post/an-introduction-to-cuda-in-python-part-3/
+    """
 
-# Select Image to Analyze
-fpath = "D:\\Dropbox\\Data\\3-3-20 glut4-vglutmeos\\" # Folder
-fname = "cell10_dz20_r2.tif" # File name
+    i,j,k = cuda.grid(3) # Get image coordinates
+    pixw = pixel_width[0]
+    m, n, o = image_in.shape # get row and column
+    if (i >= m) or ( j >= n) or (k >=o): # If Any of the threads aren't working on a part of an image kick it out
+        return
+    
 
-im = load_image_to_array(fpath + fname) # Load image into workspace
-print(im.shape) # Print shape
+    max_val = thresh[0]
+    # Prime values relative to the cuda position
+    max_i = i-pixw
+    max_j = j-pixw
+    
+    
+    # Loop over the neighborhood in a double loop
+    for ii in range(i-pixw,i+pixw):
+        for jj in range(j-pixw,j+pixw):
+            #Ensure that you are only about to look at an image pixel
+            if (ii >= 0) and (ii <m) and (jj>=0) and (jj <n):
+                #Basic maximum pixel search, record index locations
+                if image_in[ii,jj,k] >= max_val:
+                    max_val = image_in[ii,jj,k]
+                    max_i = ii
+                    max_j = jj
+    # Check to see if maximum
+    if (max_i == i) and (max_j == j):
+        image_out[i,j,k] = 1 #If local maxima turned out to be ideal
+    else:
+        image_out[i,j,k] = 0 #if not
 
+@cuda.jit()
+def gpu_image_segment(image_in, ID_image, segmentation_array, pixel_width):
+    """
+    This code will segment a 2*pixel_width+1 area around the identified pixel
+    image_in = image to be segmented
+    ID_image = 0s image with molecule ID at the center pixel of the neighborhood
+    defined as above
+    segmentation_array
+    
+
+    i,j,k = cuda.grid(3) # Get image coordinates
+    if (ID_array[])
+    """
+
+#%% Functions
+
+def localize_file(file):
+    
+    #Load Images
+    images = load_image_to_array(fpath + fname) # Load image into workspace
+    print('Image detected with m = {}, n = {}, and o = {}'.format(images.shape[0],images.shape[1],images.shape[2]))
+    
+    # Subtract Background using rolling ball subtraction
+    print('Subtracting Background')
+    gauss_sigma = 2.5
+    rolling_ball_radius = 6
+    rolling_ball_height = 6
+    images_no_background, background_images = rolling_ball_subtraction(images,  gauss_sigma, rolling_ball_radius, rolling_ball_height)
+    print('Background Subtracted')
+    
+    # Peak Identification
+    # Accepts background subtracted image and finds the likely spots for molecules  
+    # returns a binary image representing areas of likely molecular emission
+    
+    binary_peak_image = find_peaks(images_no_background, 30, 3)
+    
+    # Prepare Molecular Data Arrays
+    # Accepts binary_peak_image
+    # returns centers = nx2 array containing the central pixel of n molecular emissions
+    
+    # Image Segmentation
+    # Accepts: images_no_background, centers, pixel_width
+    # Returns: molecules = the hurricane based data structure
+    
+    
+    # Localization
+    # Accept in an array where each row represents one localization
+    
+    # Data Cleanup
+    
+    # Save relevant data
+
+def find_peaks(image, threshold, pixel_width):
+    """ Non-optimized gpu peak finding wrapper"""
+    blockdim = (32,32) #Specify number of threads in a 2D grid
+    image2 = np.empty_like(image)
+    m,n,o = image_size(image1)
+    griddim = (m// blockdim[0] + 1, n//blockdim[1] + 1,o) #Specify the block landscape
+    gpu_peaks[griddim, blockdim](np.ascontiguousarray(image1), np.ascontiguousarray(image2), np.ascontiguousarray(threshold), np.ascontiguousarray(pixel_width))
+    return image2
+
+#%% Localization Class    
+#%% Main Workspace
+if __name__ == '__main__':
+    fpath = "D:\\Dropbox\\Data\\3-3-20 glut4-vglutmeos\\" # Folder
+    fname = "cell10_dz20_r2.tif" # File name
+    #%% Load Image
+    im = load_image_to_array(fpath + fname, 20, 30) # Load image into workspace
+    print(im.shape) # Print shape
+    
+    #%% Subtract Background using rolling ball subtraction
+    print('Subtracting Background')
+    gauss_sigma = 2.5
+    rolling_ball_radius = 6
+    rolling_ball_height = 6
+    images_no_background, background_images = rolling_ball_subtraction(im,  gauss_sigma, rolling_ball_radius, rolling_ball_height)
+    print('Background Subtracted')
+    del im # No reason to keep too much memory
+    # Wavelet denoising for peak indentification
+    # Saving this for later
+    
+    #%% Peak Identification
+    #GPU Peak Identification
+    image1 = images_no_background
+    show_as_image(image1[:,:,0])
+    plt.title('Background Subtracted Frame')
+    plt.figure()
+    image2 = find_peaks(image1, 25, 5)
+    show_as_image(image2[:,:,0])
+    plt.title('Peaks')
+    # Image Segmentation
+    
+    # Localization
+    
+    # Data Cleanup
+    
+    # Save relevant data
--- Hurricane/ryan_image_io.py
+++ Hurricane/ryan_image_io.py
@@ -24,24 +24,61 @@ def image_size(image):
         o = 1
     return m,n,o
 
-def load_fits_to_array(file):
-    with fits.open(file) as im1:
-        image_stack = im1[0].data.astype('float64')
-        image_out = np.zeros((image_stack.shape[1],image_stack.shape[2],image_stack.shape[0]))
-        o,m,n = image_stack.shape
-        for i in range(o):
-            image_out[:,:,i] = image_stack[i,:,:]
-        return image_out
-        # The image is loaded in
+def load_fits_to_array(file, start=0, stop=0):
+    """
+    Allows for the loading of fits files by specified frame
+    If no frame is specified the full file is loaded as a numpy array
+    """
+    with fits.open(file) as im1: # Ensure we close the file when we're done reading the data
+        if ((start != 0) and (stop == 0)): #In this condition we want 1 frame, return the frame corresponding to start
+                im_stack = im1[0].data[start,:,:].astype('float64') # load single frame
+                # Reshape fits to expected image indexing of (m,n,o)
+                m,n = im_stack.shape 
+                o = 1
+                image_stack = np.zeros((m,n,1))
+                image_stack[:,:,0] = im_stack[:,:]
+        if (start != 0) and (stop != 0): # Want a slice of frames
+                if start != stop:
+                    image_stack = im1[0].data[start:stop+1,:,:].astype('float64') # load frames in array from start to stop
+                    # Reshape fits to expected image indexing of (m,n,o)
+                    o,m,n = image_stack.shape 
+                else:
+                    im_stack = im1[0].data[start,:,:].astype('float64') # load single frame
+                    # Reshape fits to expected image indexing of (m,n,o)
+                    m,n = im_stack.shape 
+                    o = 1
+                    image_stack = np.zeros((m,n,1))
+                    image_stack[:,:,0] = im_stack[:,:]
+        if (start == 0) and (stop != 0): # Hear we specify the end frame only
+                image_stack = im1[0].data[:stop,:,:].astype('float64') # load frames in array from start to stop
+                # Reshape fits to expected image indexing of (m,n,o)
+                o,m,n = image_stack.shape 
+        if (start == 0) and (stop == 0): # Here we want the whole data set
+                image_stack = im1[0].data.astype('float64') # load frames in array from start to stop
+                # Reshape fits to expected image indexing of (m,n,o)
+                o,m,n = image_stack.shape 
+        
+        #Preallocate Array for image out
+        image_out = np.zeros((m,n,o))
+        
+        
+        # Different rules for a single frame image
+        if o > 1:
+            for i in range(o):
+                image_out[:,:,i] = image_stack[i,:,:]
+                
+                return image_out
+        else:
+            return image_stack
 
-def load_image_to_array(file):
+def load_image_to_array(file, start = 0, stop = 0): 
     filename, file_extension = os.path.splitext(file) # split filename to check extension
     if file_extension == '.tif':
-        return load_tiff_to_array(file)
+        return load_tiff_to_array(file, start, stop)
     if file_extension == '.fits':
-        return load_fits_to_array(file)  
+        return load_fits_to_array(file, start, stop)  
     
-def load_tiff_to_array(fname):
+def load_tiff_to_array(fname, start = 0, stop = 0):
     """This function will take in a string as a file location and file name
        It will return a Numpy Array of the image at this location"""
     im = Image.open(fname) #Initial call to image datafile
@@ -51,13 +88,43 @@ def load_tiff_to_array(fname):
         m,n = np.shape(im)
         o = im.n_frames 
     except:
-        pass
+        o = 1
+    
+    if stop > o:
+        stop = 0
+    
+    if (start != 0) and (stop == 0): #In this condition we want 1 frame, return the frame corresponding to start
+        im.seek(start) #Seek to frame i in image stack
+        im_array= np.array(im)
+        m,n =im_array.shape
+        image_array = np.zeros((m,n,1))
+        image_array[:,:,0] = im_array[:,:]
         
-    # Allocate variable memory for image
-    image_array = np.zeros((m,n,o))
-    for i in range(o):
-        im.seek(i) #Seek to frame i in image stack
-        image_array[:,:,i]= np.array(im) #Populate data into preallocated variable
+    if (start != 0) and (stop != 0): # Want a slice of frames
+        if start != stop:
+            # Allocate variable memory for image
+            image_array = np.zeros((m,n,stop-start+1))
+            for i in range(stop-start+1):
+                im.seek(i+start) #Seek to frame i in image stack
+                image_array[:,:,i]= np.array(im) #Populate data into preallocated variable 
+        else:
+            im.seek(start) #Seek to frame i in image stack
+            im_array= np.array(im)
+            m,n =im_array.shape
+            image_array = np.zeros((m,n,1))
+            image_array[:,:,0] = im_array[:,:]
+    if (start == 0) and (stop != 0): # Hear we specify the end frame only
+        image_array = np.zeros((m,n,stop))
+        for i in range(stop):
+            im.seek(i) #Seek to frame i in image stack
+            image_array[:,:,i]= np.array(im) #Populate data into preallocated variable
+            
+    if (start == 0) and (stop == 0): # Here we want the whole data set
+        image_array = np.zeros((m,n,stop))
+        for i in range(o):
+            im.seek(i) #Seek to frame i in image stack
+            image_array[:,:,i]= np.array(im) #Populate data into preallocated variable          
+
     return image_array # return numpy image array
 
 def save_array_as_image(array, file):
@@ -85,10 +152,11 @@ def save_array_to_tiff(array, file):
                append_images=Im[1:])
     # This appears to work properly
     
-def show_as_image(image_frame):
+def show_as_image(image_frame, title = None):
     """We expect a 2D numpy array and will output the visual grayscale result"
     """
     plt.imshow(image_frame)
+    plt.title(title)
     
 def grab_image_files(folder):
     """Takes a list of folder contents and returns a list of images only"""
@@ -98,6 +166,6 @@ def grab_image_files(folder):
             files.append(file)
     return files
 
-#%% Main Section Workspace
+##%% Main Section Workspace
 if __name__ == '__main__':
     pass
